9d1fba5fa40b343a9660b63e1b682efd412066af
Alexander Gehr <alexander.gehr.ext@uniper.energy>
2024-09-03 12:27:34 +0200

    Added multip workflow setup

---
+++ scripts/populate-option-value.sh
@@ -0,0 +1,59 @@
+
+
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+MANUAL_TERRAFORM_YML="$SCRIPT_DIR/../.github/workflows/manual_terraform.yml"
+WORKFLOWS_DIR="$SCRIPT_DIR/../.github/workflows"
+USECASE_DIRECTORY="$SCRIPT_DIR/../usecases/"
+
+OPTIONS=`ls $USECASE_DIRECTORY`
+ 
+for WORKFLOW in `ls $WORKFLOWS_DIR`
+do
+
+    if [[ `yq '.on.workflow_dispatch.inputs.use_case.options' $WORKFLOWS_DIR/$WORKFLOW` != 'null' ]]; then
+        
+        current_options=$(yq eval '.on.workflow_dispatch.inputs.use_case.options' $WORKFLOWS_DIR/$WORKFLOW )
+    else
+        echo "skipping $WORKFLOW no options to update"
+        continue
+    fi
+
+current_options_array=()
+while read -r word; do
+    current_options_array+=("$word")
+done <<< "$current_options"
+ 
+declare -a tmp_output_array=()
+declare -a target_output_array=()
+
+for i in $OPTIONS; do
+ 
+    tmp_output_array+=("- $i")
+    target_output_array+=("$i")
+done
+
+# Check if the values in the arrays are equal
+echo ${current_options_array[*]} current values
+echo ${tmp_output_array[*]} new values
+echo "Processing $WORKFLOWS_DIR/$WORKFLOW"
+if [[ "${current_options_array[*]}" == "${tmp_output_array[*]}" ]]; then
+    echo "Values in YAML file are equal to values in array. No update needed."
+else
+    echo "Values in YAML file are not equal to values in array. Updating YAML file."
+    # Construct YAML-compatible string
+    options_string="["
+
+    for option in "${target_output_array[@]}"; do
+    echo $option
+        options_string+="\"$option\", "
+    done
+
+    options_string="${options_string%, }]"
+ 
+    echo $options_string
+    #yq -o y .on.workflow_dispatch.inputs.version.options $SOURCE_YAML_FILE
+    yq eval ".on.workflow_dispatch.inputs.use_case.options = $options_string" $WORKFLOWS_DIR/$WORKFLOW  > temp.yml && mv temp.yml $WORKFLOWS_DIR/$WORKFLOW 
+fi
+done
